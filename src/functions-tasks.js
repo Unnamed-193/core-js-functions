7bdca53b0ad28da204e4e3888c4cb396896ec840
Kirill <k187m18733@gmail.com>
2024-03-25 15:08:42 +0300

    feat: solve memoize

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -122,8 +122,17 @@ function getPolynom(...coefficients) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache = null;
+  let isCache = false;
+
+  return function cash() {
+    if (!isCache) {
+      cache = func();
+      isCache = true;
+    }
+    return cache;
+  };
 }
 
 /**
