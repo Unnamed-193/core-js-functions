40216caad184924f6e9804d25403a05941dd15d9
Kirill <k187m18733@gmail.com>
2024-03-26 15:36:22 +0300

    feat: solve getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -238,8 +238,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom;
+  return function getId() {
+    const curId = id;
+    id += 1;
+    return curId;
+  };
 }
 
 module.exports = {
